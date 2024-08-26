7c731d5636bdded7b0981a2e96a7f7f30c02c4ed
Noah Franz <noahfranz13@gmail.com>
2024-08-26 12:45:15 -0700

    setup research logging

---
+++ new-day.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get the year, month, and day
+year=$( date +%Y )
+month=$( date +%m )
+monthstr=$( date +%B )
+day=$( date +%d )
+logfile="$year/$month/$day.md"
+
+# check if the directories exist
+if [[ ! -d $year ]]; then
+    mkdir $year
+fi
+
+if [[ ! -d $year/$month ]]; then
+    mkdir $year/$month
+fi
+
+# if the file exists print an error message and exit
+if [[ -e $logfile ]]; then
+    echo "File already exists! Edit it directly!"
+    exit 1
+fi
+
+# make the file from the template
+cp template-log.txt $logfile
+sed -i "s/TODAYSDATE/$monthstr $day, $year Research Log/g" $logfile
