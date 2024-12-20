21fd9fb21abe2e4b51fc55f822b8ca36c576ec1d
Noah Franz <noahfranz13@gmail.com>
2024-12-19 19:03:43 -0500

    make scripts for opening and searching more flexible

--- open.sh
+++ open.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+Help()
+{
+    echo "Open todays research log"
+    echo
+    echo "This will, by default, open with emacs,"
+    echo "you can change it with the -e (--editor) option"
+    echo
+    echo "Options:"
+    echo "-e | --editor   The editor to use, default is emacs."
+    echo "-h | --help     Display this help message."
+    echo
+}
+
+editor=""
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -e|--editor) editor="$2"; shift 2 ;;
+    -h|--help) Help; exit ;;
+    *) echo "Invalid option: $1" >&2; exit 1 ;;
+  esac
+done
+
+if [[ -z $editor ]]; then
+    editor="emacs"
+fi
+
 # get the year, month, and day
 year=$( date +%Y )
 month=$( date +%m )
@@ -18,11 +44,11 @@ fi
 
 # if the file exists open it and exit
 if [[ -e $logfile ]]; then
-    emacs $logfile
+    $editor $logfile
     exit 1
 fi
 
 # Otherwise, make the file from the template and open it
 cp template-log.txt $logfile
 sed -i "s/TODAYSDATE/$monthstr $day, $year Research Log/g" $logfile
-emacs $logfile
+$editor $logfile
--- search.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(which w2vgrep) ]]; then
-    echo "Found w2vgrep installation, using that for search..."
-    
-else
-    echo "No w2vgrep installation found, using regular grep for search..."
-    grep -r $1
-fi
